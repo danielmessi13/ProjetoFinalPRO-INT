d8a6026a65784453d9b96851780dd48ca39bcaf9
Ermeson16 <ermeson16000@gmail.com>
2019-01-24 12:39:38 -0300

    Fix Settings

--- ProjetoFinal/Fakebook/settings.py
+++ ProjetoFinal/Fakebook/settings.py
@@ -107,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
-LANGUAGE_CODE = 'pt-BR  '
+LANGUAGE_CODE = 'pt-BR'
 
 TIME_ZONE = 'America/Sao_Paulo'
 
